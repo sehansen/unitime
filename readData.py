62c2780f85d39542c1d086a86a9ecb7af30b3622
deoxyribose <frzdyb@gmail.com>
2014-03-13 11:24:48 +0100

    comments, I think

--- readData.py
+++ readData.py
@@ -11,9 +11,9 @@ files = dict(zip(names,alldata))
 basicinfo = dict(zip(files['basic'].readline().split(" "), map(int, files['basic'].readline().split(" "))))
 
 files['courses'].readline()
-L = {}
-S = {}
-M = {}
+L = {} # A dict with key = course, and value = number of lectures
+S = {} # A dict with key = course, and value = number of students
+M = {} # A dict with key = course, and value = minimum number of days that it should be spread across
 lecturer = {}
 for line in files['courses']:
     Course, Lecturer, Number_of_lectures, Minimum_working_days, Number_of_students = tuple(line.split(" "))
@@ -22,13 +22,13 @@ for line in files['courses']:
     M[Course] = int(Minimum_working_days)
     lecturer[Course] = Lecturer
 
-C = {}
+C = {} # A dict with key = room, and value = number of students that can be in the room
 files['rooms'].readline()
 for line in files['rooms']:
     Room, Capacity = tuple(line.split(" "))
     C[Room] = int(Capacity)
 
-curric = {}
+curric = {} # A dict with key = couse, and value = list of curriculae it belongs to
 files['relation'].readline()
 for line in files['relation']:
     Curriculum, Course = tuple(line.strip('\r\n').split(" "))
@@ -46,7 +46,5 @@ for c1 in L.keys():
         if c1 == c2 or lecturer[c1] == lecturer[c2] or lists_overlap(curric[c1], curric[c2]):
             chi[(c1,c2)] = 1 # if some key isn't there, there is no conflict
 
-T = [(day, period) for day in range(1,basicinfo['Days']+1) for period in range(1,basicinfo['Periods_per_day']+1)]
-
-
-weirdgreekletter = {}
+#T = [(day, period) for day in range(1,basicinfo['Days']+1) for period in range(1,basicinfo['Periods_per_day']+1)]
+#weirdgreekletter = {}
